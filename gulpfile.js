30d21965db797b5ffa4d86bcc7b50ff51580ddb5
igorLit <Igorek96by@gmail.com>
2017-02-20 14:47:40 +0300

    context done

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,17 @@
  */
 const  gulp = require('gulp');
 const  less = require('gulp-less');
-
+const notify = require( 'gulp-notify' );
 gulp.task('default',()=>{
     console.log("default task");
 });
 
 gulp.task('less',()=> {
     return gulp.src('./assets/less/style.less')
-        .pipe(less())
+        .pipe(less().on( 'error', notify.onError({
+            title: 'Less Compilation Failed',
+            message: '<%= error.message %>'
+        })))
         .pipe(gulp.dest('./assets/css'));
 });
 gulp.task('less:watch',()=>{
